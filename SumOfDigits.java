a6c4e99b328f2b0e854f629da3c0c8067870776e
an-u-k <87515467+an-u-k@users.noreply.github.com>
2022-09-28 09:47:04 +0530

    classwork

---
+++ SumOfDigits.java
@@ -0,0 +1,30 @@
+package com.demo.bean;
+
+//sum of digits in a string
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int totalSum = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter string:");
+		String str = input.nextLine();
+		StringBuilder ch = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			//checking if  A character is digit or not
+			if (Character.isDigit(str.charAt(i))) {
+                ch.append(str.charAt(i));
+                
+				int num = Integer.parseInt(ch.toString());
+				
+				totalSum += num%10;
+				
+				
+			}
+			
+
+		}System.out.println(totalSum);
+	}
+}
